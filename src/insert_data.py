030f5117357805860434349f7eaecc528eb83c50
ConallH <107029519+ConallH@users.noreply.github.com>
2024-09-12 10:25:37 +0100

    Added def resource_path(relative_path) to insert_data

--- src/insert_data.py
+++ src/insert_data.py
@@ -1,9 +1,21 @@
 import sqlite3
 import customtkinter as ctk
+import os
+import sys
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+    
+    return os.path.join(base_path, relative_path)
 
 def connect_db():
     # Connect to the SQLite database
-    conn = sqlite3.connect('data/quiz.db')
+    conn = sqlite3.connect(resource_path('data/quiz.db'))
     cursor = conn.cursor()
     return conn, cursor
 
